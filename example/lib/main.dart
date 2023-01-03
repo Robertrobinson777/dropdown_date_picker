87585f72b932700b2305692384bea3965e976281
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2023-01-03 23:00:37 +0300

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -42,57 +42,63 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-          child: Form(
+      body: Form(
         key: formKey,
         autovalidateMode: _autovalidate,
-        child: Column(
-          children: [
-            DropdownDatePicker(
-              inputDecoration: InputDecoration(
-                  enabledBorder: const OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                  ),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10))), // optional
-              isDropdownHideUnderline: true, // optional
-              isFormValidator: true, // optional
-              startYear: 1900, // optional
-              endYear: 2020, // optional
-              width: 10, // optional
-              // selectedDay: 14, // optional
-              selectedMonth: 10, // optional
-              selectedYear: 1993, // optional
-              onChangedDay: (value) => print('onChangedDay: $value'),
-              onChangedMonth: (value) => print('onChangedMonth: $value'),
-              onChangedYear: (value) => print('onChangedYear: $value'),
-              //boxDecoration: BoxDecoration(
-              // border: Border.all(color: Colors.grey, width: 1.0)), // optional
-              // showDay: false,// optional
-              // dayFlex: 2,// optional
-              // locale: "zh_CN",// optional
-              // hintDay: 'Day', // optional
-              // hintMonth: 'Month', // optional
-              // hintYear: 'Year', // optional
-              // hintTextStyle: TextStyle(color: Colors.grey), // optional
-            ),
-            MaterialButton(
-              onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  formKey.currentState!.save();
-                  print('on save');
-                } else {
-                  print('on error');
-                  setState(() {
-                    _autovalidate = AutovalidateMode.always;
-                  });
-                }
-              },
-              child: const Text('Submit'),
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DropdownDatePicker(
+                locale: "tr",
+                inputDecoration: InputDecoration(
+                    enabledBorder: const OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                    ),
+                    helperText: '',
+                    contentPadding: const EdgeInsets.all(8),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))), // optional
+                isDropdownHideUnderline: true, // optional
+                isFormValidator: true, // optional
+                startYear: 1900, // optional
+                endYear: 2020, // optional
+                width: 10, // optional
+                // selectedDay: 14, // optional
+                selectedMonth: 10, // optional
+                selectedYear: 1993, // optional
+                onChangedDay: (value) => print('onChangedDay: $value'),
+                onChangedMonth: (value) => print('onChangedMonth: $value'),
+                onChangedYear: (value) => print('onChangedYear: $value'),
+                //boxDecoration: BoxDecoration(
+                // border: Border.all(color: Colors.grey, width: 1.0)), // optional
+                // showDay: false,// optional
+                // dayFlex: 2,// optional
+                // locale: "zh_CN",// optional
+                // hintDay: 'Day', // optional
+                // hintMonth: 'Month', // optional
+                // hintYear: 'Year', // optional
+                // hintTextStyle: TextStyle(color: Colors.grey), // optional
+              ),
+              MaterialButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    print('on save');
+                  } else {
+                    print('on error');
+                    setState(() {
+                      _autovalidate = AutovalidateMode.always;
+                    });
+                  }
+                },
+                child: const Text('Submit'),
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
