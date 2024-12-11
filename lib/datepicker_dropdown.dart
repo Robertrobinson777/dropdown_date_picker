3bc5aab800cd1b3c481bcc6868f364b6abb1eb8a
unknown <you@example.com>
2024-12-11 14:54:52 +0500

    maxHeight limit added

--- lib/datepicker_dropdown.dart
+++ lib/datepicker_dropdown.dart
@@ -34,6 +34,8 @@ class DropdownDatePicker extends StatefulWidget {
   ///Default is 12.0
   final double width;
 
+  final double? menuHeight;
+
   ///Return selected date
   ValueChanged<String?>? onChangedDay;
 
@@ -150,6 +152,7 @@ class DropdownDatePicker extends StatefulWidget {
     this.dayFlex = 1,
     this.yearFlex = 2,
     this.dateformatorder = OrderFormat.MDY,
+    this.menuHeight,
   })  : assert([
           "en",
           "zh_CN",
@@ -494,6 +497,7 @@ class _DropdownDatePickerState extends State<DropdownDatePicker> {
       hint: Text(widget.hintMonth, style: widget.hintTextStyle),
       icon: widget.icon ?? const Icon(Icons.expand_more, color: Colors.grey),
       value: monthselVal.isEmpty ? null : monthselVal,
+      menuMaxHeight: widget.menuHeight ?? 200.0,
       onChanged: (value) {
         monthSelected(value);
       },
@@ -535,6 +539,7 @@ class _DropdownDatePickerState extends State<DropdownDatePicker> {
       isExpanded: widget.isExpanded,
       icon: widget.icon ?? const Icon(Icons.expand_more, color: Colors.grey),
       value: yearselVal.isEmpty ? null : yearselVal,
+      menuMaxHeight: widget.menuHeight ?? 200.0,
       onChanged: (value) {
         yearsSelected(value);
       },
@@ -568,6 +573,7 @@ class _DropdownDatePickerState extends State<DropdownDatePicker> {
       isExpanded: widget.isExpanded,
       icon: widget.icon ?? const Icon(Icons.expand_more, color: Colors.grey),
       value: dayselVal.isEmpty ? null : dayselVal,
+      menuMaxHeight: widget.menuHeight ?? 200.0,
       onChanged: (value) {
         daysSelected(value);
       },
